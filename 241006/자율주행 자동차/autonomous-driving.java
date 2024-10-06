21a26d26c16a75399016533d3f6b30a21f288d0c
Minhyeok Song <125117389+minhyeokDev@users.noreply.github.com>
2024-10-06 18:15:19 +0900

    Title: 자율주행 자동차, Time: 98ms, Memory: 9MB, Status: Passed - CodeTree

--- 241006/자율주행 자동차/autonomous-driving.java
+++ 241006/자율주행 자동차/autonomous-driving.java
@@ -4,10 +4,9 @@ import java.util.*;
 public class Main {
 
     static int[][] map;
-
     static int n, m;
-    static int[] dx = {0, 1, 0, -1}; // 북 동 남 서
-    static int[] dy = {-1, 0, 1, 0};
+    static int[] dx = {-1, 0, 1, 0}; // 북 동 남 서
+    static int[] dy = {0, 1, 0, -1};
     static int count = 1;
 
     public static void main(String[] args) throws IOException {
@@ -46,8 +45,10 @@ public class Main {
         // 시작 위치는 방문한 도로
         map[x][y] = -1;
 
+        //현재 위치에서 현재 방향을 기준으로 왼쪽방향부터 차례대로 탐색을 진행한다.
         for (int i = 0; i < 4; i++) {
 
+            // 90도 회전
             d = (d + 3) % 4; // 왼쪽 방향으로 변환 (북 -> 서 -> 남 -> 동){
             int nX = x + dx[d];
             int nY = y + dy[d];
@@ -56,17 +57,19 @@ public class Main {
                 if (map[nX][nY] == 0) {
                     count++;
                     dfs(nX, nY, d);
+                    //여기서 return을 안하면 복귀하는 도중 뒤로가서 다른 곳을 갈수가 있음.
                     return;
                 }
             }
 
         }
-
+        // 네 방향 모두 방문했거나 인도인 경우에는
+        // 뒤쪽 칸이 인도가 아니라는 전제 하에, 바라보는 방향을 유지한 채로 한 칸 후진.
         int dir = (d + 2) % 4; // 반대 방향으로 후진
         int bX = x + dx[dir];
         int bY = y + dy[dir];
         if (bX >= 0 && bY >= 0 && bX < n && bY < m && map[bX][bY] != 1) {
-            dfs(bX, bY, d);
+            dfs(bX, bY, d); // 후진이니까 바라보는 방향은 유지
         }
 
 
